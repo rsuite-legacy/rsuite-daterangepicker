9c66f7b5aa380dadce55d8e592a8713f440d265a
simonguo <simonguo.2009@gmail.com>
2017-04-26 18:40:19 +0800

    Update docs

--- docs/examples/DateRangePicker.js
+++ docs/examples/DateRangePicker.js
@@ -2,26 +2,58 @@ import React from 'react';
 import moment from 'moment';
 import DateRangePicker from '../../src';
 
-export default () => {
-  const today = moment().startOf('day');
-  const yesterday = today.clone().subtract(1, 'd');
-  return (
-    <div className="doc-example">
-      <DateRangePicker
-        defaultEndDate={new Date}
-        defaultStateDate={new Date}
-        placement="bottomRight"
-        ranges={[
-          { label: '最近一周', range: [yesterday.clone().subtract(7, 'd'), yesterday] },
-          { label: '最近 30 天', range: [yesterday.clone().subtract(30, 'd'), yesterday] }
-        ]}
-        onChange={(beginDate, endDate) => {
-          console.log(beginDate, endDate);
-        }}
-        messages={{
-          week: ['日', '一', '二', '三', '四', '五', '六']
-        }}
-      />
-    </div>
-  );
-};
+export default React.createClass({
+  getInitialState() {
+    return {
+      startDate: null,
+      endDate: null
+    }
+  },
+
+  render() {
+    const { startDate, endDate } = this.state;
+    const today = moment().startOf('day').toDate();
+
+    const nextMaxDate = startDate ? moment(startDate).clone().add(10, 'd').toDate() : null;
+    const nextMinDate = endDate ? moment(endDate).clone().subtract(10, 'd').toDate() : null;
+    const maxDate = nextMaxDate > today ? today : nextMaxDate;
+    const minDate = endDate ? nextMinDate : null;
+
+
+    return (
+      <div className="doc-example">
+        <DateRangePicker
+          defaultStartDate={startDate}
+          defaultEndDate={endDate}
+          maxDate={maxDate}
+          minDate={minDate}
+          placement="bottomRight"
+          ranges={[
+            { label: '最近一周', range: [moment().startOf('day').clone().subtract(7, 'd'), today] },
+            { label: '最近 30 天', range: [moment().startOf('day').clone().subtract(30, 'd'), today] }
+          ]}
+          onSelect={(date, type) => {
+            this.setState({
+              [`${type}Date`]: date
+            });
+          }}
+          onChange={(startDate, endDate) => {
+            this.setState({
+              startDate,
+              endDate
+            });
+          }}
+          onClear={() => {
+            this.setState({
+              startDate: null,
+              endDate: null
+            });
+          }}
+          messages={{
+            week: ['日', '一', '二', '三', '四', '五', '六']
+          }}
+        />
+      </div>
+    );
+  }
+});
--- docs/md/props.md
+++ docs/md/props.md
@@ -7,5 +7,6 @@ maxDate | Date | | 最大可选时间
 minDate | Date | | 最小可选时间
 ranges | array | [今日, 昨天, 本周, 本月] | 快捷选项， 可以自定义选值范围
 placement | string|  `bottomRight` | 日历弹出位置: `bottomLeft` `bottomCenter` `bottomRight` `topLeft` `topCenter` `topRight`
-onChange| func | | 日期改变后的回调函数 返回 `beginDate` `endDate` 两个值
+onChange| function | | 日期改变后的回调函数，返回 `beginDate` `endDate` 两个值
+onSelect | function | | 在日历面板上选择后的回调函数，返回 `date`, `type` 两个值
 messages | object |  | 国际化对应的语言描述
