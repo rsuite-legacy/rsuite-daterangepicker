7954a5be2debb4a728ccdce70e6dda941bb92393
simonguo <simon.guo@hypers.com>
2017-11-07 18:38:30 +0800

    Update test case

--- test/DatePickerSpec.js
+++ test/DatePickerSpec.js
@@ -17,11 +17,75 @@ describe('DatePicker', () => {
     assert.ok(findDOMNode(instance).className.match(/\brsuite-daterangepicker-calendar\b/));
   });
 
+
+
   it('Should output a date', () => {
     const instance = ReactTestUtils.renderIntoDocument(
       <DatePicker calendarDate={[moment('2017-08'), moment('2017-09')]} index={0} />
     );
     assert.equal(findDOMNode(instance).querySelector('.calendar-header-title ').innerText, '2017-08');
   });
+
+  it('Should call `onChangeCalendarDate` callback', (done) => {
+
+    const instance = ReactTestUtils.renderIntoDocument(
+      <DatePicker
+        calendarDate={[moment('2017-08'), moment('2017-09')]}
+        index={0}
+        onChangeCalendarDate={() => {
+          done();
+        }}
+      />
+    );
+
+    ReactTestUtils.Simulate.click(findDOMNode(instance).querySelector('.calendar-header-backward'));
+  });
+
+  it('Should call `onChangeCalendarDate` callback', (done) => {
+
+    const instance = ReactTestUtils.renderIntoDocument(
+      <DatePicker
+        calendarDate={[moment('2017-08'), moment('2017-10')]}
+        index={0}
+        onChangeCalendarDate={() => {
+          done();
+        }}
+      />
+    );
+    ReactTestUtils.Simulate.click(findDOMNode(instance).querySelector('.calendar-header-forward'));
+  });
+
+  it('Should call `onChangeCalendarDate` callback', (done) => {
+
+    const instance = ReactTestUtils.renderIntoDocument(
+      <DatePicker
+        calendarDate={[moment('2017-08'), moment('2017-10')]}
+        index={0}
+        onChangeCalendarDate={() => {
+          done();
+        }}
+      />
+    );
+    ReactTestUtils.Simulate.click(findDOMNode(instance).querySelector('.month-dropdown-month-cell'));
+  });
+
+
+  it('Should change calendarState', () => {
+
+    const instance = ReactTestUtils.renderIntoDocument(
+      <DatePicker
+        calendarDate={[moment('2017-08'), moment('2017-10')]}
+        index={0}
+      />
+    );
+    instance.toggleMonthDropdown(() => {
+      assert.equal(instance.state.calendarState, 'DROP_MONTH');
+    });
+
+    instance.toggleMonthDropdown(() => {
+      assert.equal(instance.state.calendarState, null);
+    });
+  });
+
 
 });
