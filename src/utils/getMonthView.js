d22bacd8a6cf8aa6818533774dbef2e539b15247
simonguo <simon.guo@hypers.com>
2018-07-10 22:54:24 +0800

    Fix: Calendar renders incomplete

--- src/utils/getMonthView.js
+++ src/utils/getMonthView.js
@@ -5,16 +5,24 @@
  */
 export default function getMonthView(monthDate, isoWeek) {
   let firstDayOfMonth = monthDate.day();
-  let distance = isoWeek ? 1 - firstDayOfMonth : 0 - firstDayOfMonth;
-  let firstWeekendDate = monthDate.clone().add(distance, 'days');
+  let distance = 0 - firstDayOfMonth;
 
+  if (isoWeek) {
+    distance = 1 - firstDayOfMonth;
+
+    if (firstDayOfMonth === 0) {
+      distance = -6;
+    }
+  }
+
+  let firstWeekendDate = monthDate.clone().add(distance, 'days');
   let weeks = [firstWeekendDate];
   let nextWeekendDate = firstWeekendDate.clone().add(7, 'days');
 
-  weeks.push(nextWeekendDate.format('YYYY-MM-DD'));
+  weeks.push(nextWeekendDate);
   while (weeks.length < 6) {
     nextWeekendDate = nextWeekendDate.clone().add(7, 'days');
-    weeks.push(nextWeekendDate.format('YYYY-MM-DD'));
+    weeks.push(nextWeekendDate);
   }
 
   return weeks;
